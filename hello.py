4df94f73bcd2f86355b0ca58774bb2d026451703
unknown <joshycarovlogs@gmail.com>
2024-09-26 14:16:21 +0800

    Lab-3

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+first_name = "Josh"
+last_name = "Ycaro"
+print(first_name)
+print(last_name)
+
+print(first_name, last_name)
+print(first_name+" "+ last_name)
+
+num = 5
+if num>2:
+    print("yey")
---
+++ lab2.py
@@ -0,0 +1,10 @@
+first_name = "Josh"
+last_name = "Ycaro"
+age = str(18)
+fullname = first_name+ " "+last_name
+print("Hello, my name is "+fullname+ ". I am "+age+" Years old.")
+if(fullname.upper == False):
+    print(fullname)
+else:
+    new_caps = fullname.upper()
+    print(new_caps)
